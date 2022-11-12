2e9329ea9a6e4657ea3e81ec8caffe0ef5f66665
Shengzhou Qiang <53031172+Alephant6@users.noreply.github.com>
2022-11-12 15:51:19 +0800

    Update basic.cy.js
    修改报错

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Blog');
+    cy.get('h1').contains('Nothing But Myself');
   });
 });
